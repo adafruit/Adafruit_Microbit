9d6a7d625ba0fa5000d46ae28d917ef79f3e24da
ladyada <limor@ladyada.net>
2017-09-20 02:05:31 -0400

    fix plotter

--- examples/ble_plotterdemo/ble_plotterdemo.ino
+++ examples/ble_plotterdemo/ble_plotterdemo.ino
@@ -1,21 +1,13 @@
 // Copyright (c) Sandeep Mistry. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
-
-// Import libraries (BLEPeripheral depends on SPI)
-#include <SPI.h>
-#include <BLEPeripheral.h>
-#include "BLESerial.h"
-
+#include <Adafruit_Microbit.h>
 // Add accelerometer support
 #include "Wire.h"
 #include "MMA8653.h"
 
+Adafruit_Microbit microbit;
 MMA8653 accel;
-
-// Our BLE connection
-BLESerial bleSerial;
-
 
 void setup() {
   Serial.begin(115200);
@@ -25,13 +17,15 @@ void setup() {
   accel.begin(false, 2); // 8-bit mode, 2g range
     
   // custom services and characteristics can be added as well
-  bleSerial.setLocalName("microbit UART");
-  bleSerial.begin();
+  microbit.BTLESerial.begin();
+  microbit.BTLESerial.setLocalName("microbit");
+
+  microbit.begin();
 }
 
 void loop() {
-  bleSerial.poll();
-
+  microbit.BTLESerial.poll();
+  
   accel.update();
 
   // print the data on serial port
@@ -40,9 +34,11 @@ void loop() {
   Serial.println(accel.getZ());
 
   // send it over bluetooth
-  bleSerial.print(accel.getX());    bleSerial.print(",");
-  bleSerial.print(accel.getY());    bleSerial.print(",");
-  bleSerial.println(accel.getZ());
+  microbit.BTLESerial.print(accel.getX());   
+  microbit.BTLESerial.print(",");
+  microbit.BTLESerial.print(accel.getY());   
+  microbit.BTLESerial.print(",");
+  microbit.BTLESerial.println(accel.getZ());
 
   delay(100);
 }
