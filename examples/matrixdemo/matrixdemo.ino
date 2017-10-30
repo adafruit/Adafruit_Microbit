0dbfdca497f5f6f1862d0d8c15b088c08cf4f759
ladyada <limor@ladyada.net>
2017-10-30 00:13:32 -0400

    matrix fix and pop into arduino lib manager

--- examples/matrixdemo/matrixdemo.ino
+++ examples/matrixdemo/matrixdemo.ino
@@ -24,57 +24,57 @@ void setup() {
   microbit.begin();
 
   // setup font for later!
-  microbit.setFont(&TomThumb);
-  microbit.setTextWrap(false);
-  microbit.setTextColor(LED_ON);
+  microbit.matrix.setFont(&TomThumb);
+  microbit.matrix.setTextWrap(false);
+  microbit.matrix.setTextColor(LED_ON);
 }
     
 void loop(){
   // Fill screen
-  microbit.fillScreen(LED_ON);
+  microbit.matrix.fillScreen(LED_ON);
   delay(1000);
 
   // draw a face
-  microbit.clear();
-  microbit.drawBitmap(0, 0, smile_bmp, 8, 5, LED_ON);
+  microbit.matrix.clear();
+  microbit.matrix.drawBitmap(0, 0, smile_bmp, 8, 5, LED_ON);
   delay(1000);
   
-  microbit.clear();
+  microbit.matrix.clear();
   // Draw a line 'by hand'
-  microbit.drawPixel(0, 0, LED_ON);
-  microbit.drawPixel(1, 1, LED_ON);
-  microbit.drawPixel(2, 2, LED_ON);
-  microbit.drawPixel(3, 3, LED_ON);
-  microbit.drawPixel(4, 4, LED_ON);
+  microbit.matrix.drawPixel(0, 0, LED_ON);
+  microbit.matrix.drawPixel(1, 1, LED_ON);
+  microbit.matrix.drawPixel(2, 2, LED_ON);
+  microbit.matrix.drawPixel(3, 3, LED_ON);
+  microbit.matrix.drawPixel(4, 4, LED_ON);
   // draw the 'opposite' line with drawline (easier!)
-  microbit.drawLine(0, 4, 4, 0, LED_ON);
+  microbit.matrix.drawLine(0, 4, 4, 0, LED_ON);
 
   delay(1000);
 
   // erase screen, draw a square
-  microbit.clear();
-  microbit.drawRect(0,0, 5, 5, LED_ON); // top left corner @ (0,0), 5 by 5 pixels size
+  microbit.matrix.clear();
+  microbit.matrix.drawRect(0,0, 5, 5, LED_ON); // top left corner @ (0,0), 5 by 5 pixels size
 
   delay(1000);
 
   // erase screen, draw a circle
-  microbit.clear();
-  microbit.drawCircle(2,2, 2, LED_ON); // center on 2, 2, radius 2
+  microbit.matrix.clear();
+  microbit.matrix.drawCircle(2,2, 2, LED_ON); // center on 2, 2, radius 2
 
   delay(1000);
 
   // erase screen, draw a filled triangle
-  microbit.clear();
-  microbit.fillTriangle(0,4, 2,0, 4,4, LED_ON); 
+  microbit.matrix.clear();
+  microbit.matrix.fillTriangle(0,4, 2,0, 4,4, LED_ON); 
 
   delay(1000);
 
   // scroll some text
   String myMessage = "HELLO WORLD";
   for (int i = 5; i > (((int)myMessage.length()-1) * -5) ; i--) {
-    microbit.setCursor(i, 5);
-    microbit.clear();
-    microbit.print(myMessage);
+    microbit.matrix.setCursor(i, 5);
+    microbit.matrix.clear();
+    microbit.matrix.print(myMessage);
     delay(150);
   }
 }
