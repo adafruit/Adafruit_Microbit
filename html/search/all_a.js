0157786bb489319d55e70137f2776136818af7c4
Doxygen CI <ci-arduino@invalid>
2021-07-07 17:19:31 +0000

    Deploy docs to GitHub Pages from commit cd30251fd9
    Commit: cd30251fd931cde54c8fb6e6e7456241e8f4d712
    GitHub Actions run: 1008858225

--- html/search/all_a.js
+++ html/search/all_a.js
@@ -4,5 +4,5 @@ var searchData=
   ['matrix_5fcols',['MATRIX_COLS',['../_adafruit___microbit_8cpp.html#a19d8c543f14d754cafd731d930bdc3a1',1,'Adafruit_Microbit.cpp']]],
   ['matrix_5frows',['MATRIX_ROWS',['../_adafruit___microbit_8cpp.html#a237af2b6cf547aaf4d0de5bc5a538672',1,'Adafruit_Microbit.cpp']]],
   ['max_5fprecision',['MAX_PRECISION',['../_adafruit___microbit_8cpp.html#a4ed9e805da6b82cc662002e83173a2b8',1,'Adafruit_Microbit.cpp']]],
-  ['microbit_5fsmile',['MICROBIT_SMILE',['../_adafruit___microbit_8cpp.html#a4bbe6bfbbc2ea20ff53046e960ff48cb',1,'Adafruit_Microbit.cpp']]]
+  ['microbit_5fsmile',['MICROBIT_SMILE',['../class_adafruit___microbit___matrix.html#a063dc941dcc2a3f29894b484b88b42fc',1,'Adafruit_Microbit_Matrix']]]
 ];
--- html/search/variables_4.js
+++ html/search/variables_4.js
@@ -1,5 +1,5 @@
 var searchData=
 [
   ['matrix',['matrix',['../class_adafruit___microbit.html#ae9fbf8d7d80c1dc260a5cadf6c0d4c6d',1,'Adafruit_Microbit']]],
-  ['microbit_5fsmile',['MICROBIT_SMILE',['../_adafruit___microbit_8cpp.html#a4bbe6bfbbc2ea20ff53046e960ff48cb',1,'Adafruit_Microbit.cpp']]]
+  ['microbit_5fsmile',['MICROBIT_SMILE',['../class_adafruit___microbit___matrix.html#a063dc941dcc2a3f29894b484b88b42fc',1,'Adafruit_Microbit_Matrix']]]
 ];
